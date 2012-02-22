28bbe414c569a6093bb972a83fee1aec399dd1d2
Jason R <pm.promethium@gmail.com>
2012-02-22 12:28:05 -0800

    bookmarks version 2

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,19 +4,21 @@ class Bookmark
     field :title, :type => String
     field :description, :type => String
     field :tags, :type => Array
-    field :privacy, :type => String
-    field :userID, :type => String
+    field :is_private, :type => Boolean
+    field :user_id, :type => Integer
     field :timestamp, :type => Time
 
+    belongs_to :user
+
     validates :url, :presence => true,
-                    :length => {:minimum => 2}
+                    :length => {:minimum => 2, :message => "must be at least 2 characters long"}
 
     validates :title, :presence => true,
-                      :length => {:minimum => 2}
+                      :length => {:minimum => 2, :message => "must be at least 2 characters long"}
+                      :length => {:maximum => 255, :message => "must be less than 256 characters long"}
 
-    validates :userID, :presence => true,
-                       :length => {:minimum => 2},
-                       :format => { :with => /^[A-Za-z\d_]+$/, :message => "can only be alphanumeric with no spaces"}
+    validates :user_id, :presence => true,
+                       :length => {:minimum => 1, :message => "must be at least 1 characters long"}
 
     validates :timestamp, :presence => true
 

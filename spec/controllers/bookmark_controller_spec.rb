a47aec4ed7d6c8d1986d0625d48748c51ba1ca51
Jason R <pm.promethium@gmail.com>
2012-04-03 19:24:09 -0700

    basic bookmark tests

---
+++ spec/controllers/bookmark_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe BookmarkController do
+
+end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Bookmark do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe Bookmark do
+
+it { should accept_values_for(:url, "janedoe", "jd", "jd23", "j_d") }
+it { should_not accept_values_for(:url, “”, “j”, nil)}
+
+it { should accept_values_for(:title, "test", "tt", "t11", "t_est") }
+it { should_not accept_values_for(:title, "”, “j”, nil)}
+
+it { should accept_values_for(:description, "testing","balh blah blah", "jd 125", "jd_123", nil) }
+
 end
